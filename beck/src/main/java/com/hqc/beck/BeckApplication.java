9a84bc88eb4655448affaed145c9132679d065b1
Marco Fabrini <fabrinimarco@protonmail.com>
2025-03-07 18:20:10 +0100

    init

---
+++ beck/src/main/java/com/hqc/beck/BeckApplication.java
@@ -0,0 +1,13 @@
+package com.hqc.beck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeckApplication.class, args);
+	}
+
+}
---
+++ beck/src/test/java/com/hqc/beck/BeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqc.beck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
